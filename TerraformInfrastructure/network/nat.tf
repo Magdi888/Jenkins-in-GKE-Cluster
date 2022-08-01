5e012f3043ab8ad950801b3e531849efd9524a96
Ahmed Magdi <a.magdi888@gmail.com>
2022-08-01 13:53:13 +0200

    change nat

--- TerraformInfrastructure/network/nat.tf
+++ TerraformInfrastructure/network/nat.tf
@@ -14,32 +14,22 @@ resource "google_compute_router_nat" "nat" {
   region                             = var.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
-
+ 
   subnetwork {
     name                    = google_compute_subnetwork.management-subnetwork.name
     source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
   }
-
-  log_config {
-    enable = true
-    filter = "ERRORS_ONLY"
-  }
-}
-
-resource "google_compute_router_nat" "restricted-nat" {
-  name                               = "gke-node-nat"
-  router                             = google_compute_router.router.name
-  region                             = var.region
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
 
   subnetwork {
     name                    = google_compute_subnetwork.restricted-subnetwork.name
     source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
   }
 
+
   log_config {
     enable = true
     filter = "ERRORS_ONLY"
   }
 }
+
+
